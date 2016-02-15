afafe55d7550d3988e40e6de8635c49f14fc9609
Kosta Korenkov <7r0ggy@gmail.com>
2016-02-15 18:05:16 +0300

    Don't handle bitcoin: schema

--- src/js/services/uriHandler.js
+++ src/js/services/uriHandler.js
@@ -6,9 +6,9 @@ UriHandler.prototype.register = function() {
   var base = window.location.origin + '/';
   var url = base + '#/uri-payment/%s';
 
-  if(navigator.registerProtocolHandler) {
-    navigator.registerProtocolHandler('bitcoin', url, 'Copay');
-  }
+/*  if(navigator.registerProtocolHandler) {
+    navigator.registerProtocolHandler('bitcoin', url, 'urv2');
+  }*/
 };
 
 angular.module('copayApp.services').value('uriHandler', new UriHandler());
